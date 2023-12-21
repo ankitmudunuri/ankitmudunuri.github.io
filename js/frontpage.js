a7b0efdaa4eccdcd9f0a20767274ec449eb2bf2e
ankitmudunuri <ankit.mudunuri@gmail.com>
2023-12-20 19:51:04 -0500

    testing for mobile

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,18 +1,1 @@
-
-function mobileCheck(){
-let details = navigator.userAgent;
-let regexp = /android|iphone|kindle|ipad/i; 
-  
-let isMobileDevice = regexp.test(details); 
-  
-if (isMobileDevice) { 
-    document.getElementsByClassName("main_header1").style.visibility = "hidden";
-    document.getElementsByClassName("main_header2").style.visibility = "visible";
-    document.getElementsByClassName("topsection_mainpage").style.backgroundPosition = "50% -70px";
-    document.getElementsByClassName("profile").style.top = "80%";
-    document.getElementsByClassName("profile").style.left = "8.5%";
-    document.getElementsByClassName("description").style.paddingLeft = "5%";
-    document.getElementsByClassName("description").style.paddingRight = "5%";
-    document.getElementsByClassName("description").style.top = "95%";
-}
-}
+// nothing to see here
